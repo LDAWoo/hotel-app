c3a4726a13c634119be3ec01ea228b85580cafc9
LDAVu2k3 <leducanhvu3006@gmail.com>
2024-03-22 08:55:59 +0700

    Update Border.jsx

--- joinstaying/src/components/Border/Border.jsx
+++ joinstaying/src/components/Border/Border.jsx
@@ -1,13 +1,7 @@
 import PropTypes from "prop-types";
 
 const Border = ({ className }) => {
-  return (
-    <hr
-      className={`w-auto h-[1px] border-primary-50 dark:border-primary-400 ${
-        className && className
-      }`}
-    />
-  );
+  return <hr className={`w-auto h-[1px] border-primary-50 dark:border-primary-400 ${className && className}`} />;
 };
 
 Border.propTypes = {

9ba1b1146a850a1e7b4bb17dc65a75be2c1a22bd
LDAWoo <leducanhvu3006@gmail.com>
2024-04-09 18:52:05 +0700

    update

--- client/src/pages/BookingTrip/MyTrip/MyTrip.jsx
+++ client/src/pages/BookingTrip/MyTrip/MyTrip.jsx
@@ -4,7 +4,7 @@ import { getBooking } from "../../../api/User/Booking";
 import Title from "../../../components/Title/Title";
 import Image from "../../../components/Image/Image";
 import Button from "../../../components/Buttons/Button";
-import {format} from 'date-fns'
+import {format, setDate} from 'date-fns'
 import { getLocale } from "../../../components/Locale/Locale";
 import { Link } from "react-router-dom";
 import MoneyFormatStaying from "../../../components/Staying/MoneyFormatStaying";
@@ -28,8 +28,10 @@ function MyTrip() {
                 try {
                     setLoading(true)
                     const results = await getBooking(token)
+                    setData(results.data)
                     setLoading(false)
                 } catch (error) {
+                    setData([])
                     setLoading(true)
                 }
             }
@@ -50,6 +52,8 @@ function MyTrip() {
         setVisible(!visible);
     }
 
+    console.log(data);
+
     return ( 
         <div className="flex w-full">
             <div className="flex flex-col gap-12 w-full">
@@ -62,60 +66,64 @@ function MyTrip() {
                         ) : (
                             <>
                                 <div className="flex flex-col gap-1 w-full">
-                                    <div className="flex flex-col gap-1">
-                                        <Title title="Ho Chi Minh City" fontBold extraLarge9 nowrap={false} />
-                                        <Title title={`${format(new Date(), 'dd LLLL yyyy', {locale})} - ${format(new Date(), 'dd LLLL yyyy', {locale})}`} xxl nowrap={false} />
-                                    </div>
-
-                                    <Link to={routesConfig.mybooking} target="_blank" className="relative z-0 mt-4 flex w-full rounded-md border border-gray-200 p-6 duration-200 shadow-[0_2px_8px_0_rgba(26,26,26,0.16)] hover:shadow-[0_2px_16px_0_rgba(26,26,26,0.24)]">
-                                        <div className="flex flex-row items-center w-full">
-                                            <div className="flex flex-row w-full">
-                                                <div className="flex flex-row items-start sm:items-center flex-1 gap-6">
-                                                    <div className="min-w-[64px] min-h-[64px] max-w-[64px] max-h-[64px] 2md:w-[80px] 2md:h-[80px] 2md:max-w-[80px] 2md:max-h-[80px] opacity-60 bg-white">
-                                                        <Image src="https://bstatic.com/xdata/images/hotel/300x300/504430246.jpg?k=e4423874ca62d56e8cd030e1fdac03a7587d8fb99fdc95dbbb52f84bb51c2bb1&o="/>
-                                                    </div>
-                                                    <div className="flex flex-col gap-1 text-gray-500">
-                                                        <Title title="Hotel" fontBold xxl />
-                                                        <div className="inline sm:flex flex-row items-center gap-1">
-                                                            <Title title={`${format(new Date(), 'dd LLLL yyyy', {locale})}`} xl nowrap={false}/>
-                                                            <Title title={`${"–"}`} xl nowrap={false} className="m-[0_4px] sm:m-0"/>
-                                                            <Title title={`${format(new Date(), 'dd LLLL yyyy', {locale})}`} xl nowrap={false}/>
-                                                            <Title title={`${"·"}`} xl nowrap={false} className="m-[0_4px] sm:m-0"/>
-                                                            <Title title={`${"Ho Chi Minh"}`} xl nowrap={false} className="2md:flex hidden"/>
-                                                            <MoneyFormatStaying price={200000} decimalScale={0} className="2md:hidden gap-1 text-[14px]"/>
-                                                        </div>
-                                                        <Title title={t("MyTrip.complete")} xl/>
-                                                    </div>
-                                                </div>
-
-                                                <div className="flex flex-row items-start">
-                                                    <div className="flex flex-row items-center gap-2">
-                                                        <MoneyFormatStaying price={200000} decimalScale={0} className="2md:flex hidden font-bold gap-1 text-[18px]"/>
-                                                        <ToolTip width={230} placement="bottom-end" isVisible={visible} onClickOutside={handleShowOptions} typeToolTip="TippyHeadless" interactive items={
-                                                            <div className="pt-2 pb-2 overflow-hidden rounded-md shadow-[0_2px_8px_0_rgba(26,26,26,0.16)]">
-                                                                <div className="flex flex-col w-full">
-                                                                    {
-                                                                        items.map((item,index) => (
-                                                                            <Link to={routesConfig.hotelDetails} target="_blank" key={index} className="p-[10px_16px] hover:bg-gray-100 duration-300">
-                                                                                <Title title={item?.name} xl/>
-                                                                            </Link>
-                                                                        ))
-                                                                    }
-                                                                </div>
-                                                            </div>
-                                                        }>
-                                                            <div>
-                                                                <Button icon={SlOptionsVertical} size={16} onClick={(e) => {
-                                                                    e.preventDefault();
-                                                                    handleShowOptions();
-                                                                }} className="p-2 bg-transparent hover:bg-gray-100 duration-300 rounded-sm" classButton="ml-1 mt-1 mb-1"/>
-                                                            </div>
-                                                        </ToolTip>
-                                                    </div>
-                                                </div>
+                                    {data.map((item, index) => (
+                                        <div key={index}>
+                                            <div className="flex flex-col gap-1">
+                                                <Title title={`${item?.address}`} fontBold extraLarge9 nowrap={false} />
+                                                <Title title={`${format(new Date(), 'dd LLLL yyyy', {locale})} - ${format(new Date(), 'dd LLLL yyyy', {locale})}`} xxl nowrap={false} />
                                             </div>
+
+                                            <Link to={`${routesConfig.mybooking}?bid=${item?.bookingId}`} target="_blank" className="relative z-0 mt-4 flex w-full rounded-md border border-gray-200 p-6 duration-200 shadow-[0_2px_8px_0_rgba(26,26,26,0.16)] hover:shadow-[0_2px_16px_0_rgba(26,26,26,0.24)]">
+                                                <div className="flex flex-row items-center w-full">
+                                                    <div className="flex flex-row w-full">
+                                                        <div className="flex flex-row items-start sm:items-center flex-1 gap-6">
+                                                            <div className="min-w-[64px] min-h-[64px] max-w-[64px] max-h-[64px] 2md:w-[80px] 2md:h-[80px] 2md:max-w-[80px] 2md:max-h-[80px] opacity-60 bg-white">
+                                                                <Image src="https://bstatic.com/xdata/images/hotel/300x300/504430246.jpg?k=e4423874ca62d56e8cd030e1fdac03a7587d8fb99fdc95dbbb52f84bb51c2bb1&o="/>
+                                                            </div>
+                                                            <div className="flex flex-col gap-1 text-gray-500">
+                                                                <Title title="Hotel" fontBold xxl />
+                                                                <div className="inline sm:flex flex-row items-center gap-1">
+                                                                    <Title title={`${format(new Date(), 'dd LLLL yyyy', {locale})}`} xl nowrap={false}/>
+                                                                    <Title title={`${"–"}`} xl nowrap={false} className="m-[0_4px] sm:m-0"/>
+                                                                    <Title title={`${format(new Date(), 'dd LLLL yyyy', {locale})}`} xl nowrap={false}/>
+                                                                    <Title title={`${"·"}`} xl nowrap={false} className="m-[0_4px] sm:m-0"/>
+                                                                    <Title title={`${item?.address}`} xl nowrap={false} className="2md:flex hidden"/>
+                                                                    <MoneyFormatStaying price={200000} decimalScale={0} className="2md:hidden gap-1 text-[14px]"/>
+                                                                </div>
+                                                                <Title title={t("MyTrip.complete")} xl/>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="flex flex-row items-start">
+                                                            <div className="flex flex-row items-center gap-2">
+                                                                <MoneyFormatStaying price={200000} decimalScale={0} className="2md:flex hidden font-bold gap-1 text-[18px]"/>
+                                                                <ToolTip width={230} placement="bottom-end" isVisible={visible} onClickOutside={handleShowOptions} typeToolTip="TippyHeadless" interactive items={
+                                                                    <div className="pt-2 pb-2 overflow-hidden rounded-md shadow-[0_2px_8px_0_rgba(26,26,26,0.16)]">
+                                                                        <div className="flex flex-col w-full">
+                                                                            {
+                                                                                items.map((item,index) => (
+                                                                                    <Link to={routesConfig.hotelDetails} target="_blank" key={index} className="p-[10px_16px] hover:bg-gray-100 duration-300">
+                                                                                        <Title title={item?.name} xl/>
+                                                                                    </Link>
+                                                                                ))
+                                                                            }
+                                                                        </div>
+                                                                    </div>
+                                                                }>
+                                                                    <div>
+                                                                        <Button icon={SlOptionsVertical} size={16} onClick={(e) => {
+                                                                            e.preventDefault();
+                                                                            handleShowOptions();
+                                                                        }} className="p-2 bg-transparent hover:bg-gray-100 duration-300 rounded-sm" classButton="ml-1 mt-1 mb-1"/>
+                                                                    </div>
+                                                                </ToolTip>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </Link>
                                         </div>
-                                    </Link>
+                                    ))}
                                 </div>
                             </>
                         )
--- client/src/pages/Successfully/Successfully.jsx
+++ client/src/pages/Successfully/Successfully.jsx
@@ -1,13 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Button from "../../components/Buttons/Button";
+import Image from "../../components/Image/Image";
 import Title from "../../components/Title/Title";
+import routesConfig from '../../configs/routesConfig';
 
 const Successfully = () => {
+  const {t} = useTranslation();
+  const navigate = useNavigate();
+
+  const handleBackToHome = () => {
+    navigate(routesConfig.home)
+  }
+
   return (
-    <div className='w-full'>
-      <div className='w-full m-auto lg:max-w-[var(--max-width)] mt-10 p-[10px] bg-transparent'>
+    <div className='w-full flex-1 flex'>
+      <div className='w-full m-auto lg:max-w-[var(--max-width)] mt-10 mb-10 p-[10px] bg-transparent'>
         <div className='flex flex-col w-full'>
           <div className="flex flex-col gap-4">
-            <div className="flex flex-row justify-center">
-              <Title title="We have created room for you, so you can find the room that suits you." fontMedium extraLarge5 />
+            <div className="flex flex-col justify-center text-center">
+              <Title title={t("BookingSuccess.title")} fontBold extraLarge6 nowrap={false}/>
+              <Title title={t("BookingSuccess.subTitle")} fontMedium extraLarge5 nowrap={false}/>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <div className="w-[200px] h-[200px] 2md:min-w-[250px] 2md:min-h-[250px]">
+                  <Image src="/images/hotel-floor.png" className="min-w-full min-h-full"/>
+                </div>
+            </div>
+
+            <div className="flex w-auto justify-center items-center">
+                <div>
+                  <Button title={t("BookingSuccess.backToHome")} background className="p-[8px_16px] rounded-sm" xxl fontMedium onClick={handleBackToHome}/>
+                </div>
             </div>
           </div>
         </div>
